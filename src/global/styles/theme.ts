6c520bb65012323f74d293f693f97fbdca795321
OtavioFilipe <darksnack16@gmail.com>
2022-07-19 19:27:01 -0300

    Initial config and create Welcome page

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+        background: '#FFFFFF',
+        backgroundWater: '#58ABF6',
+        text_white: '#FFFFFF',
+
+        types: {
+            water: '#4A90DA'
+        }
+    }
+};
---
+++ src/pages/Welcome/index.tsx
@@ -0,0 +1,30 @@
+import AnimatedLottieView from "lottie-react-native";
+import React from "react";
+import pokemonAnimation from "../../assets/images/pokemon.json";
+import { Button } from "../../components/Button";
+
+import * as S from "./styles";
+
+export function Welcome() {
+  return (
+    <S.Container>
+      <S.Content>
+        <S.WrapperAnimation>
+          <S.WrapperImage>
+            <AnimatedLottieView
+              style={{ width: 200 }}
+              autoPlay
+              source={pokemonAnimation}
+              loop
+            />
+          </S.WrapperImage>
+        </S.WrapperAnimation>
+        <S.Title>Bem Vindo</S.Title>
+        <S.SubTitle>Encontre todos os pokémons em um só lugar</S.SubTitle>
+      </S.Content>
+      <S.Footer>
+        <Button title="Iniciar" />
+      </S.Footer>
+    </S.Container>
+  );
+}
