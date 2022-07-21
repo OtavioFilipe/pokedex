fe8c2f6bc69fa662fb4c5ebe68dac16f5caf9171
OtavioFilipe <darksnack16@gmail.com>
2022-07-20 21:28:58 -0300

    add animation card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 import { Card, Pokemon, PokemonType } from "../../components/Card";
+import { FadeAnimation } from "../../components/FadeAnimation";
 import api from "../../services/api";
 
 import * as S from "./styles";
@@ -51,7 +52,11 @@ export function Home() {
       <FlatList
         data={pokemons}
         keyExtractor={(pokemon) => pokemon.id.toString()}
-        renderItem={({ item: pokemon }) => <Card data={pokemon} />}
+        renderItem={({ item: pokemon }) => (
+          <FadeAnimation>
+            <Card data={pokemon} />
+          </FadeAnimation>
+        )}
       />
     </S.Container>
   );
